4e6ee169236a39423fa1120d088c52d623b73a39
s-merzj <111936225+s-merzj@users.noreply.github.com>
2024-06-04 08:16:01 -0700

    EnemyBehaviour.cs

--- Scripts-Ext/EnemyBehaviour.cs
+++ Scripts-Ext/EnemyBehaviour.cs
@@ -5,43 +5,75 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1f;
+    [SerializeField] bool destroyOnContact = false;
+    [SerializeField] public bool spawnFlipped = false;
+    [SerializeField] bool flipOnColliderEdge = true;
 
     Rigidbody2D myRigidBody;
-    BoxCollider2D myBoxCollider;
+    Collider2D myBoxCollider;
     public int damage = 6;
     public bool moveRight = true;
-    void Start(){
+    private float spawnTime;
+    public bool isProj = false;
+    public float projectileDist = 99999;
+    private float spawnPos = 0;
+    
+
+    void Start() {
+        if(!moveRight){
+            this.gameObject.transform.Rotate(0, 180, 0, Space.World);
+        }
         myRigidBody = GetComponent<Rigidbody2D>();
-        myBoxCollider = GetComponent<BoxCollider2D>();   
+        myBoxCollider = GetComponent<Collider2D>();  
+        spawnPos = this.gameObject.transform.position.x;
+        if (spawnFlipped) {this.moveRight = !this.moveRight;} 
+        this.spawnTime = Time.time;
     }
-    void Update(){
+
+    void Update() {
        if(IsFacingRight()) {
             //move right hehehehe
             myRigidBody.velocity = new Vector2(moveSpeed,0f);
-       } 
-       else 
-       {
+       } else {
             myRigidBody.velocity = new Vector2(-(moveSpeed),0f);
        }
+       if(Mathf.Abs(this.gameObject.transform.position.x - spawnPos) > projectileDist && isProj){
+        Destroy(this.gameObject);
+       }
     }
-    private bool IsFacingRight(){
-        return (transform.localScale.x > Mathf.Epsilon) && moveRight;
-        
+
+    private bool IsFacingRight() {
+        return ((transform.localScale.x > Mathf.Epsilon) && moveRight);
     }
-    private void OnTriggerExit2D(Collider2D collision){
-        if (collision.gameObject.CompareTag("Ground")){
-            transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)*2), transform.localScale.y);
-            moveRight = (moveRight!);
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (this.flipOnColliderEdge) {
+            if (collision.gameObject.CompareTag("Ground") && moveRight) {
+                transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)*2), transform.localScale.y);
+                moveRight = (moveRight!);
+            } else {
+                transform.localScale = new Vector2((Mathf.Sign(myRigidBody.velocity.x)*2), transform.localScale.y);
+                moveRight = true;
+            }
         }
     }
-    void OnCollisionEnter2D(Collision2D col){
+
+    void OnCollisionEnter2D(Collision2D col) {
         
         GameObject go = col.gameObject;
-        if (go.CompareTag("Player"))
-        {
+        if (go.CompareTag("Player")) {
             PlayerHealth playerHealth = go.GetComponent<PlayerHealth>();
             playerHealth.TakeDamage(damage);
         }
 
+        this.ContactEvent(col);
+    }
+
+    public bool ContactEvent(Collision2D col) {
+        if (this.destroyOnContact && col.gameObject.tag != "Spawner" && col.gameObject.tag != "Enemy") {
+            Destroy(this.gameObject);
+            return true;
+        }
+        return false;
     }
 }
