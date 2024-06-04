d35d87231f850a6ce2edd088b738e09a72da9b1a
s-merzj <111936225+s-merzj@users.noreply.github.com>
2024-06-04 08:18:58 -0700

    PlayerMovement.cs

--- Scripts-Ext/PlayerMovement.cs
+++ Scripts-Ext/PlayerMovement.cs
@@ -1,52 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
- 
+
 public class PlayerMovement : MonoBehaviour
 {
-    //fuckitweball
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 17f;
+    [SerializeField]private float speed = 8f;
+    private float jumpingPower = 16f;
     private bool isFacingRight = true;
-    
-    //next few are for double jump
-    private bool doubleJump;
-    
-        [SerializeField] private Rigidbody2D rb;
-        [SerializeField] private Transform groundCheck;
-        [SerializeField] private LayerMask groundLayer;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform flippedGroundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private bool spawnGravFlipped;
+
+    private bool gravFlipped = false;
+
+    void Start() {
+        SetGravFlip(!spawnGravFlipped);
+    }
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        if(IsGrounded() && !Input.GetButton("Jump")){
-            doubleJump = false;
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower * ((gravFlipped) ? -1 : 1));
         }
-        if(Input.GetButtonDown("Jump") && IsGrounded || doubleJump ()){
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-            doubleJump = !doubleJump;
-        }
-        if(Input.GetButtonUp("Jump") && rb.velocity.y>0f()){
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-            
         }
+
         Flip();
     }
-    private void FixedUpdate(){
+
+    private void FixedUpdate()
+    {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
-    private bool IsGrounded(){
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
 
-
-    }
-    private void Flip(){
-        if(isFacingRight&&horizontal<0f||!isFacingRight&&horizontal>0f){
-            isfacingright = !isfacingright;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;   
+    private bool IsGrounded()
+    {
+        if (!gravFlipped) {
+            return Physics2D.OverlapCircle(groundCheck.position, 0.35f, groundLayer);
+        } else {
+            return Physics2D.OverlapCircle(flippedGroundCheck.position, 0.35f, groundLayer);
         }
     }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    public void SetGravFlip(bool gravDown) {
+        gravFlipped = !gravDown;
+        rb.gravityScale = Mathf.Abs(rb.gravityScale) * ((gravFlipped) ? -1 : 1); // Flip gravity
+        transform.Find("Anchor/Player sprite").GetComponent<SpriteRenderer>().flipY = gravFlipped; // Flip the sprite vertically
+    }
+
+    public bool gravityFlipped {get => gravFlipped; set => SetGravFlip(!value);}
 }
